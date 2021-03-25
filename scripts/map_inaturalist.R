9926babd04c465198b53af498eab0ebe869f1221
bsaul <bradleysaul@gmail.com>
2021-03-25 10:54:59 -0400

    adds inaturalist maps

--- scripts/map_inaturalist.R
+++ scripts/map_inaturalist.R
@@ -10,16 +10,18 @@ ecoregions <- sf::st_transform(ecoregions, proj)
 piedmont   <- ecoregions[ecoregions$NA_L3CODE == "8.3.4", ]
 bbox_pied  <- sf::st_bbox(piedmont)
 
+clip_box <- bbox_pied + (c(-1, -1, 1, 1)*1000000)
+
 usa <- 
   maps::map('state', plot = FALSE, fill = TRUE) %>%
   sf::st_as_sf() %>%
   sf::st_transform(proj) %>%
   sf::st_make_valid() %>%
-  sf::st_crop(bbox_pied + (c(-1, -1, 1, 1)*1000000))
+  sf::st_crop(clip_box)
 
 get_inaturalist_obs <- function(taxon, .proj = proj){
   rinat::get_inat_obs(
-    taxon_id = 83893,
+    taxon_id = taxon,
     place_id = "united-states",
     quality = "research", 
     maxresults = 10000) %>%
@@ -27,7 +29,8 @@ get_inaturalist_obs <- function(taxon, .proj = proj){
       coords = c("longitude", "latitude"), 
       crs = sf::st_crs("+proj=longlat +datum=WGS84")
     ) %>%
-    sf::st_transform(.proj)
+    sf::st_transform(.proj) %>%
+    sf::st_crop(clip_box)
 }
 
 
@@ -41,10 +44,30 @@ map_taxon <- function(taxon){
     theme_void()
 }
 
-p <- map_taxon("83893")
-ggsave(p, file = "static/images/map-83893.png", 
-       width = 500, height = 500,
-       units = "mm")
+plants <- c(
+  `Parthenium integrolium`      = 83893,
+  `Asclepias tuberosa`          = 47912,
+  `Coreopsis major`             = 129672,
+  `Marshallia obovata`          = 129317,
+  `Quercus marilandica`         = 82329,
+  `Schizachyrium scoparium`     = 122603,
+  `Sorghastrum nutans`          = 122608,
+  `Symphyotrichum grandiflorum` = 169466
+)
+
+
+purrr::walk(
+  .x = plants,
+  .f = ~ {
+    print(sprintf("Mapping %s", .x))
+    p <- map_taxon(.x)
+    ggsave(p, file = sprintf("static/images/map-%s.png", .x), 
+           width = 500, height = 500,
+           units = "mm")
+  }
+)
+
+
 
 
 
