9327401a4f97206a450db170026621a57b6fb503
Bradley Saul <bradleysaul@fastmail.com>
2024-07-21 10:09:36 -0400

    Nix updates (#76)
    * update nix; remove renv
    
    * remove script

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
-  description = "A flake for the piedmontprairie.us site ";
+  description = "A flake for the piedmontprairie.us site";
   nixConfig = {
     bash-prompt = "> ";
   };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,6 +12,30 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      
+      formatter = pkgs.nixpkgs-fmt;
+      
+      packages.site = pkgs.stdenv.mkDerivation {
+        name = "piedmontprairie.us";
+        buildInputs = with pkgs; 
+          [R
+           hugo
+           pandoc 
+           texliveSmall 
+           rPackages.blogdown
+           rPackages.bibtex
+          ];
+        src = ./.;
+        buildPhase = ''
+          ${pkgs.R}/bin/Rscript -e "blogdown::build_site(build_rmd = TRUE)"
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r public/ $out/
+        '';
+
+      };
+
       devShells.default =  pkgs.mkShell {
         buildInputs = [
           pkgs.bashInteractive 
@@ -23,8 +47,11 @@
           pkgs.hugo
           pkgs.updog
 
+          pkgs.rPackages.blogdown
           pkgs.rPackages.devtools
           pkgs.R
+
+          pkgs.pandoc
           pkgs.libpng
         ];
       }; 
--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-Rscript -e "blogdown::build_site(build_rmd = TRUE)"
